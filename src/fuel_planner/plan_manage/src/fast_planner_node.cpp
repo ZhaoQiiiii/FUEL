fef77efd403f6dc660aa8f89b248227b3306ebac
ZhaoQiiiii <wzqwzq1024@163.com>
2026-03-04 14:16:39 +0800

    s1

--- src/fuel_planner/plan_manage/src/fast_planner_node.cpp
+++ src/fuel_planner/plan_manage/src/fast_planner_node.cpp
@@ -2,8 +2,8 @@
 #include <visualization_msgs/Marker.h>
 
 #include <plan_manage/kino_replan_fsm.h>
-#include <plan_manage/topo_replan_fsm.h>
 #include <plan_manage/local_explore_fsm.h>
+#include <plan_manage/topo_replan_fsm.h>
 
 #include <plan_manage/backward.hpp>
 namespace backward {
@@ -12,27 +12,27 @@ backward::SignalHandling sh;
 
 using namespace fast_planner;
 
-int main(int argc, char** argv) {
-  ros::init(argc, argv, "fast_planner_node");
-  ros::NodeHandle nh("~");
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "fast_planner_node");
+    ros::NodeHandle nh("~");
 
-  int planner;
-  nh.param("planner_node/planner", planner, -1);
+    int planner;
+    nh.param("planner_node/planner", planner, -1);
 
-  TopoReplanFSM topo_replan;
-  KinoReplanFSM kino_replan;
-  LocalExploreFSM local_explore;
+    TopoReplanFSM topo_replan;
+    KinoReplanFSM kino_replan;
+    LocalExploreFSM local_explore;
 
-  if (planner == 1) {
-    kino_replan.init(nh);
-  } else if (planner == 2) {
-    topo_replan.init(nh);
-  } else if (planner == 3) {
-    local_explore.init(nh);
-  }
+    if (planner == 1) {
+        kino_replan.init(nh);
+    } else if (planner == 2) {
+        topo_replan.init(nh);
+    } else if (planner == 3) {
+        local_explore.init(nh);
+    }
 
-  ros::Duration(1.0).sleep();
-  ros::spin();
+    ros::Duration(1.0).sleep();
+    ros::spin();
 
-  return 0;
+    return 0;
 }
